24b6939b2648d86c2add404fa7e8f5d0a49054cd
jonathan_libonati <libonati.jonathan@gmail.com>
2022-02-25 21:49:52 -0300

    progress in nav

--- scripts/curtain.js
+++ scripts/curtain.js
@@ -2,11 +2,14 @@ const hambur = document.getElementById("but01")
 const nav__list = document.getElementById("nav__list01")
 const nav__curtblur = document.getElementById("nav__curtain01")
 const nav__navblur = document.getElementById("nav__blur01")
+const body = document.getElementById("body")
 
 function curtain() {
 	nav__list.classList.toggle("showOpa100")
 	nav__curtblur.classList.toggle("showOpa90")
 	nav__navblur.classList.toggle("showOpa90")
+	body.classList.toggle("overFlowHidden")
+
 }
 
 hambur.addEventListener("click", curtain)
