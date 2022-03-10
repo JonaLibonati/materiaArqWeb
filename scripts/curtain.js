aa1a934a5671fb6ca0f2add64e057a81605beb9d
jonathan_libonati <libonati.jonathan@gmail.com>
2022-03-10 00:09:02 -0300

    nav fixes

--- scripts/curtain.js
+++ scripts/curtain.js
@@ -6,8 +6,15 @@ const body = document.getElementById("body")
 const pj = document.getElementById("pj01")
 const about = document.getElementById("about01")
 const pj__link = document.getElementById("pj__link01")
+const nav__item = document.getElementsByClassName("nav__item")
+const nav__itemFirst = document.getElementsByClassName("nav__item--first")[0]
 
 function curtain() {
+	nav__itemFirst.classList.toggle("displayNone")
+	nav__item[0].classList.toggle("displayNone")
+	nav__item[1].classList.toggle("displayNone")
+	nav__item[2].classList.toggle("displayNone")
+	nav__item[3].classList.toggle("displayNone")
 	nav__list.classList.toggle("showOpa100")
 	nav__list.classList.toggle("height100vh")
 	nav__list.classList.toggle("zIndex70")
